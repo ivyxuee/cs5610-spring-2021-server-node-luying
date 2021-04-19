1fd28e3415b1892d0bbf63b1c9af1174c93553a4
ivyxue <xue.lu@husky.neu.edu>
2021-04-18 23:30:02 -0700

    update

---
+++ controllers/quizzes-controller 2.js
@@ -0,0 +1,16 @@
+const quizService = require('../services/quiz-service')
+
+module.exports = (app) => {
+  const findAllQuizzes = (req, res) =>
+    quizService.findAllQuizzes()
+      .then(allQuizzes => res.json(allQuizzes));
+
+  const findQuizById = (req, res) => {
+    const quizId = req.params['qid'];
+    quizService.findQuizById(quizId)
+      .then(quiz => res.json(quiz));
+  };
+
+  app.get('/api/quizzes', findAllQuizzes);
+  app.get('/api/quizzes/:qid', findQuizById);
+}
